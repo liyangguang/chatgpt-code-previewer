089a7f53b346360f7499515b09f04d33f281d454
Yangguang Li <liyangguangcn@gmail.com>
2023-04-02 20:49:45 -0700

    Change to use interval. CSS tweaks.

--- script-openai.js
+++ script-openai.js
@@ -4,27 +4,16 @@ function _log(message) {
 
 (function start() {
   _log('start')
-  listenToResize();
+  listenToChange();
 })();
 
-// Using resize as a signal of new message
-function listenToResize() {
-  const resizeObserver = new ResizeObserver(() => {
-    _log('resize');
-    setTimeout(() => {
-      if (isReady()) injectUI();
-    }, 1000)
-  });
-  
-  // Not the best selector. But hard to find a good one in tailwind.
-  const scrollEl = document.querySelector('main > div > div > div > div');
-  resizeObserver.observe(scrollEl);
-  _log('listener attached')
-}
-
-function isReady() {
-  // The current message finished rendering.
-  return document.querySelector('form button').textContent.includes('Regenerate response');
+function listenToChange() {
+  _log('listening to change')
+  setInterval(() => {
+    if (document.querySelector('form button').textContent.includes('Regenerate response')) {
+      injectUI();
+    }
+  }, 1000);
 }
 
 function injectUI() {
@@ -39,55 +28,68 @@ function injectUI() {
     iframe.srcdoc = code;
     preEl.append(iframe)
 
-    // Inject toggle button
-    const controls = document.createElement('div');
-    controls.classList.add('toggle');
+    const toggleEl = generateToggle();
+    const codepenEl = generateCodepenLink();
 
-    // Left label
-    const leftLabel = document.createElement('span');
-    leftLabel.classList.add('label-text');
-    leftLabel.textContent = 'Code';
-    controls.append(leftLabel);
-
-    // Input
-    const labelEl = document.createElement('label');
-    labelEl.classList.add('switch');
-    const inputEl = document.createElement('input');
-    inputEl.type = 'checkbox';
-    inputEl.checked = true;
-    const insideSpanEl = document.createElement('span');
-    insideSpanEl.classList.add('slider');
-    insideSpanEl.classList.add('round');
-    labelEl.append(inputEl);
-    labelEl.append(insideSpanEl);
-    inputEl.addEventListener('change', () => {
-      iframe.classList.toggle('-hide');
-    })
-    controls.append(labelEl);
-
-    // Left label
-    const rightLabel = document.createElement('span');
-    rightLabel.classList.add('label-text');
-    rightLabel.textContent = 'Preview';
-    controls.append(rightLabel);
-
-    const codepenEl = document.createElement('a');
-    codepenEl.textContent = 'Open in CodePen';
-    codepenEl.href = 'https://codepen.io/pen/';
-    codepenEl.target = '_blank';
-    codepenEl.addEventListener('click', () => {
-      const type = preEl.querySelector('span').textContent.toLowerCase();
-      const {htmlString, styleString, scriptString} = parseCode(code, type);
-      console.log('htmlString', htmlString);
-      console.log('styleString', styleString);
-      console.log('scriptString', scriptString);
-      // TODO: save to storage
-    });
-    controls.append(codepenEl);
-
-    preEl.append(controls);
+    const controlRow = preEl.querySelector('div > div');
+    controlRow.classList.add('control-row');
+    const titleEl = controlRow.querySelector('span');
+    titleEl.classList.add('control-title');
+    titleEl.after(toggleEl, codepenEl);
     _log(`done one`);
   }
+}
+
+function generateToggle() {
+  const toggleEl = document.createElement('div');
+  toggleEl.classList.add('toggle');
+
+  // Left label
+  const leftLabel = document.createElement('span');
+  leftLabel.classList.add('label-text');
+  leftLabel.textContent = 'Code';
+  toggleEl.append(leftLabel);
+
+  // Input
+  const labelEl = document.createElement('label');
+  labelEl.classList.add('switch');
+  const inputEl = document.createElement('input');
+  inputEl.type = 'checkbox';
+  inputEl.checked = true;
+  const insideSpanEl = document.createElement('span');
+  insideSpanEl.classList.add('slider');
+  insideSpanEl.classList.add('round');
+  labelEl.append(inputEl);
+  labelEl.append(insideSpanEl);
+  inputEl.addEventListener('change', () => {
+    iframe.classList.toggle('-hide');
+  })
+  toggleEl.append(labelEl);
+
+  // Left label
+  const rightLabel = document.createElement('span');
+  rightLabel.classList.add('label-text');
+  rightLabel.textContent = 'Preview';
+  toggleEl.append(rightLabel);
+
+  return toggleEl;
+}
+
+function generateCodepenLink() {
+  const codepenEl = document.createElement('a');
+  codepenEl.textContent = 'Open in CodePen';
+  codepenEl.href = 'https://codepen.io/pen/';
+  codepenEl.target = '_blank';
+  codepenEl.classList.add('codepen');
+  codepenEl.addEventListener('click', () => {
+    const type = preEl.querySelector('span').textContent.toLowerCase();
+    const {htmlString, styleString, scriptString} = parseCode(code, type);
+    console.log('htmlString', htmlString);
+    console.log('styleString', styleString);
+    console.log('scriptString', scriptString);
+    // TODO: save to storage
+  });
+  return codepenEl;
 }
 
 function scanHtmlBlock() {
